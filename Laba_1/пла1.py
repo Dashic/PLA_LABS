b3121f04a1799d7413e7c8f114fccbe53a3ab69a
Doshik <stedasha28@mail.ru>
2024-10-31 13:34:11 +0300

    new

--- Laba_1/пла1.py
+++ Laba_1/пла1.py
@@ -68,8 +68,53 @@ p = encoding(word)
 # 1 0 0 0 0
 
 p[0][1] = 0
-p[5][2] = 1
+p[3][2] = 1
 p[2][4] = 1
 p[5][1] = 1
         
-print(encoding(word))
+
+H = np.array([[1, 1, 0, 1, 1, 0, 0],
+              [1, 1, 1, 0, 0, 1, 0],
+              [1, 0, 1, 1, 0, 0, 1]])
+
+
+k = [] #Список с номерами столбцов с ошибками
+s = [] #Список с ошибками
+
+for i in range(5):
+    if str(np.transpose(decoding(word))[i]) != str(np.array([0, 0, 0])):
+        k += [i+1]
+        s = [list(np.transpose(decoding(word))[i])] + s
+
+
+d = [] #Список с номерами строк с ошибок
+for j in range(len(s)):
+    for y in range(7):
+        if str(np.array(s[j])) == str(np.transpose(H)[y]):
+            d += [y-1]
+
+for i in range(len(k)):
+    p[k[i]][d[i]] ^= 1
+
+end_word = [list(p[1])]
+for i in range(3,6):
+    end_word += [list(p[i])]
+
+list_end = []
+for i in range(4):
+    list_end += end_word[i]
+bin_list_end = [str(i) for i in list_end]
+bin_end = ''.join(bin_list_end)
+
+last_list = []
+for i in range(0,20,5):
+    last_list += [bin_end[i:i+5]]
+    
+reversed_alp_code = {value: key for key, value in alp_cod.items()}
+
+word_end_list = [reversed_alp_code[i] for i in last_list]
+word_end = ''.join(word_end_list)
+print(word_end)
+    
+
+        
